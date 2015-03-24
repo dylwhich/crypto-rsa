6ae2a112e1001342c211f259391960159e385a9c
Dylan Whichard <dylan@whichard.com>
2015-03-24 16:28:50 -0400

    Fix adjacent face algorithm

--- mesh.c
+++ mesh.c
@@ -3,23 +3,24 @@
 size_t Face_adjacent(struct Mesh *mesh, size_t face,
 		     size_t *adjacent, size_t max_results) {
   size_t results = 0;
-  size_t cur_edge = mesh->edges[mesh->faces[face].edge].next;
+  size_t cur_edge = mesh->faces[face].edge;
 
-  while (cur_edge != mesh->faces[face].edge) {
+  do {
     if (results == max_results) {
       return 0;
     }
 
-    adjacent[results] = mesh->edges[cur_edge].pair;
+    adjacent[results] = mesh->edges[mesh->edges[cur_edge].pair].face;
     cur_edge = mesh->edges[cur_edge].next;
     results++;
-  }
+  }  while (cur_edge != mesh->faces[face].edge);
 
   return results;
 }
 
 void HEdge_faces(struct Mesh *mesh, size_t edge, size_t *face_a, size_t *face_b) {
-  
+  *face_a = mesh->edges[edge].face;
+  *face_b = mesh->edges[mesh->edges[edge].pair].face;
 }
 
 size_t Vertex_faces(struct Mesh *mesh, size_t vertex,
